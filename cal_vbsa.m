9b9803a8e2142bb53e749e9a90013223cfd03c66
Lingfeng Zhou <lingfeng.zhou.cn@gmail.com>
2020-05-10 22:18:46 +0800

    Update cla_vbsa.m

--- cal_vbsa.m
+++ cal_vbsa.m
@@ -25,10 +25,11 @@ YC = mean(YT((2*N+1):end,:),2);
 [ Si, STi ] = vbsa_indices(YA,YB,YC);
 
 % Plot results:
+X_Labels = cell(1,nNum);
 for ii=1:nNum
     X_Labels{ii} = Extra.Parlist.Symbol(Extra.Parlist.parameter_location(ii));
 end
-X_Labels = {'Sm','beta','alfa','Rs','Rf'} ;
+% X_Labels = {'Sm','beta','alfa','Rs','Rf'} ;
 figure % plot main and total separately
 subplot(121); boxplot1(Si,X_Labels,'main effects')
 subplot(122); boxplot1(STi,X_Labels,'total effects')
